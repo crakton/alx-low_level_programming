80e0b84b27a0df1060e62e76f4e7909a846d58f0
Jrcity <redemptionjonathan1@gmail.com>
2023-06-20 18:52:02 +0100

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
- * @main -print _putchar
- * @return 0 on success
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	_putchar("_putchar");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
