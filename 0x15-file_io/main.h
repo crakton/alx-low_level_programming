08eb2dd1beea5eda0c42aaaa2e77b710d8a03e3e
crakton <redemptionjonathan1@gmail.com>
2024-02-12 23:42:30 +0100

    100-elf_header header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,6 @@ int create_file(const char *filename, char *text_content);
 int _strlen(const char *s);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
+void cp_file(char *file_from, char *file_to);
 void close_file(int fd);
 #endif /* _MAIN_H_ */
