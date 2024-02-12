6dc0f1c8dff3197a5ba61d3b72d01317f00b2ccd
crakton <redemptionjonathan1@gmail.com>
2024-02-12 23:50:48 +0100

    100-elf_header header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(const char *s);
-char *create_buffer(char *file);
-ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void cp_file(char *file_from, char *file_to);
-void close_file(int fd);
-#endif /* _MAIN_H_ */
+
+#endif /*MAIN_H*/
